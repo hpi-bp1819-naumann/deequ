b80c4f988140aa275ecfa037d27515cd08b8e860
Malte <malte.barth@student.hpi.de>
2018-11-07 13:07:50 +0100

    sql statement works correctly but conerting the result to a byte array raises an exception

---
+++ src/main/scala/com/amazon/deequ/analyzers/jdbc/JdbcApproxQuantile.scala
@@ -0,0 +1,88 @@
+/**
+  * Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  *
+  * Licensed under the Apache License, Version 2.0 (the "License"). You may not
+  * use this file except in compliance with the License. A copy of the License
+  * is located at
+  *
+  *     http://aws.amazon.com/apache2.0/
+  *
+  * or in the "license" file accompanying this file. This file is distributed on
+  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+  * express or implied. See the License for the specific language governing
+  * permissions and limitations under the License.
+  *
+  */
+
+package com.amazon.deequ.analyzers.jdbc
+
+import java.sql.ResultSet
+import java.nio.ByteBuffer
+
+import com.amazon.deequ.analyzers.Analyzers.{metricFromFailure, metricFromValue}
+import com.amazon.deequ.analyzers.{Analyzers, ApproxQuantileState}
+import com.amazon.deequ.analyzers.runners.EmptyStateException
+import com.amazon.deequ.metrics.{DoubleMetric, Entity}
+import org.postgresql.util.PSQLException
+import Preconditions.isNumeric
+import org.apache.spark.sql.catalyst.expressions.aggregate.ApproximatePercentile
+
+case class JdbcApproxQuantile(column: String, quantile: Double)
+  extends JdbcAnalyzer[ApproxQuantileState, DoubleMetric] {
+
+  override def validateParams(table: Table, column: String): Unit = {
+    super.validateParams(table, column)
+
+    isNumeric(table, column)
+  }
+  override def computeStateFrom(table: Table): Option[ApproxQuantileState] = {
+
+    val connection = table.jdbcConnection
+
+    validateParams(table, column)
+
+    val query =
+      s"""
+         |SELECT
+         | PERCENTILE_DISC($quantile) WITHIN GROUP ( ORDER BY $column ) AS approx_quantile
+         |FROM
+         | ${table.name}
+      """.stripMargin
+
+    val statement = connection.prepareStatement(query, ResultSet.TYPE_FORWARD_ONLY,
+      ResultSet.CONCUR_READ_ONLY)
+
+    val result = statement.executeQuery()
+
+    try {
+      result.next()
+    }
+    catch {
+      case error: PSQLException => throw error
+    }
+
+    try {
+      val approx_quantile = ApproximatePercentile.serializer.deserialize(result.getBytes("approx_quantile")) // this still raises a java.nio.BufferUnderflowException
+
+      Some(ApproxQuantileState(approx_quantile))
+    }
+    catch {
+      case error: Exception => throw error
+    }
+  }
+
+  override def computeMetricFrom(state: Option[ApproxQuantileState]): DoubleMetric = {
+    state match {
+      case Some(theState) =>
+        val percentile = theState.percentileDigest.getPercentiles(Array(quantile)).head
+        Analyzers.metricFromValue(percentile, "ApproxQuantile", column)
+      case _ =>
+        toFailureMetric(new EmptyStateException(
+          s"Empty state for analyzer JdbcApproxQuantile, all input values were NULL."))
+    }
+  }
+
+  override private[deequ] def toFailureMetric(failure: Exception) = {
+    metricFromFailure(failure, "ApproxQuantile", column, Entity.Column)
+  }
+}
